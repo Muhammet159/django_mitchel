96f034944c7cb21ad4e06da1e8631db423ecab4f
admin <rehnetli.damja@mail.ru>
2019-07-21 15:03:06 +0500

    second commit

--- personal/views.py
+++ personal/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+from account.models import Account
+
 def home_screen_view(request):
-	return render(request, "personal/home.html", {})
+	context = {}
+
+	accounts = Account.objects.all()
+	context['accounts'] = accounts
+	return render(request, "personal/home.html", context)
